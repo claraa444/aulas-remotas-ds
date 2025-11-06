11da1c32714d9c0920812b311ddc158e6c1eed64
claraa444 <clarasiqueiraneumann@gmail.com>
2025-11-06 11:56:25 -0300

    feat: exercise

--- atividade1.py
+++ atividade1.py
@@ -1,1 +1,8 @@
+nome = input("Qual é o seu nome?")
+idade_texto = input("Qual a sua idade?")
+idade_num = int(idade_texto)
+  if idade_num >= 18:
+    print("Olá, {nome}, você é maior de idade.")
+  else:
+    print("Olá, {nome}, você não é maior de idade.")
 
