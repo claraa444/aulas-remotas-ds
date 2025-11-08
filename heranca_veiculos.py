754d938059f10883f0f362d063dfe614525139fc
claraa444 <clarasiqueiraneumann@gmail.com>
2025-11-08 12:25:52 -0300

    exercicio

--- heranca_veiculos.py
+++ heranca_veiculos.py
@@ -1,1 +1,11 @@
+class Veiculo: #classe mae
+    def __init__(self, cor, modelo, ano, marca):
+      self.cor = cor
+      self.modelo = modelo
+      self.ano = 0
+      self.marca = marca
 
+class Carro(Veiculo): #classe filha
+    def __init__(self, cor, modelo, ano, marca, numero_portas):
+      super().__init__(cor, modelo, ano, marca)
+      self.numero_portas = 0 
