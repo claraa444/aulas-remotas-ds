a84aab7613697df96b3f5025af945ea4d43a4050
claraa444 <clarasiqueiraneumann@gmail.com>
2025-11-08 13:07:05 -0300

    calculadora

--- funcao_calculadora.py
+++ funcao_calculadora.py
@@ -1,1 +1,10 @@
+def calcular_soma(numero_1, numero_2, numero_3):
+   
+    resultado = numero_1 + numero_2 + numero_3
+    print(f"A soma desses números é {resultado}")
 
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite o segundo número: "))
+numero_3 = int(input("Digite o terceiro número: "))
+    
+calcular_soma(numero_1, numero_2, numero_3)
